f0bf70dea21a8106ac4f4d5595d3671cedf5ef19
星尘AI-Labs <niupidi@foxmail.com>
2026-07-30 20:15:21 +0800

    chore: auto-sync 2026-07-30 20:15:20

--- promo-video/src/RootV2.tsx
+++ promo-video/src/RootV2.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Composition} from "remotion";
 import {HubAwakeningRebuild} from "./v2/HubAwakeningRebuild";
 import {CodeWorkflowJourney} from "./v2/CodeWorkflowJourney";
+import {IntentToResults} from "./v2/IntentToResults";
 
 export const RootV2: React.FC = () => (
   <>
@@ -17,6 +18,14 @@ export const RootV2: React.FC = () => (
       id="CodeWorkflowJourney"
       component={CodeWorkflowJourney}
       durationInFrames={240}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+    <Composition
+      id="IntentToResults"
+      component={IntentToResults}
+      durationInFrames={210}
       fps={30}
       width={1920}
       height={1080}
